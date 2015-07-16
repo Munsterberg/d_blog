4c58dc74348f0c1bdaea9e3c9ac6ecc54b840790
Munsterberg <jetrussell93@gmail.com>
2015-07-16 06:49:54 -0400

    Blog detail view working as intended

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-  url(r'^$', views.blog_list, name='blog_list')
+  url(r'^$', views.blog_list, name='blog_list'),
+  url(r'(?P<pk>\d+)/$', views.blog_detail, name='blog_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Article, Comment
 
@@ -7,4 +7,10 @@ def blog_list(request):
   articles = Article.objects.all()
   return render(request, 'blog/blog_list.html', {
     'articles': articles
+  })
+
+def blog_detail(request, pk):
+  article = get_object_or_404(Article, pk=pk)
+  return render(request, 'blog/blog_detail.html', {
+    'article': article
   })
