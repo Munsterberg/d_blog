ce8f9bee634f6e8d05f6393a664979a5def33acb
Munsterberg <jetrussell93@gmail.com>
2015-07-16 21:50:26 -0400

    Comments displaying for each article

--- blog/views.py
+++ blog/views.py
@@ -4,13 +4,15 @@ from .models import Article, Comment
 
 # Create your views here.
 def blog_list(request):
-  articles = Article.objects.all()
+  articles = Article.objects.all().order_by("-created_at")
   return render(request, 'blog/blog_list.html', {
     'articles': articles
   })
 
 def blog_detail(request, pk):
   article = get_object_or_404(Article, pk=pk)
+  comments = Comment.objects.filter(article_id=pk)
   return render(request, 'blog/blog_detail.html', {
     'article': article,
+    'comments': comments,
   })
