4e8398920749106a54475c88155e98225e076575
Munsterberg <jetrussell93@gmail.com>
2015-07-17 05:25:14 -0400

    Comment form redirecting as intended

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect, render_to_response
 from django.http import HttpResponseRedirect
 
 from .models import Article, Comment
@@ -19,9 +19,7 @@ def blog_detail(request, pk):
       comment = form.save(commit=False)
       comment.article_id = pk
       comment.save()
-      return redirect('blog_detail')
-    #else:
-      #print(form.errors)
+      return HttpResponseRedirect('/blog/%s' % pk)
   else:
     form = CommentForm()      
 
